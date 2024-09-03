0f56f580179858a737dfe77237d1a0182a198e4a
Than0s <himanshupatil45h@gmail.com>
2024-09-03 14:41:35 +0530

    request list screen structure created

--- app/src/main/java/dev/than0s/aluminium/features/register/presentation/screens/registration_requests/RequestsScreen.kt
+++ app/src/main/java/dev/than0s/aluminium/features/register/presentation/screens/registration_requests/RequestsScreen.kt
@@ -1,1 +1,20 @@
 package dev.than0s.aluminium.features.register.presentation.screens.registration_requests
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RegistrationRequests() {
+
+}
+
+@Composable
+private fun RegistrationRequestsContent() {
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun RegistrationRequestPreview() {
+
+}
