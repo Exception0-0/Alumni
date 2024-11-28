9000195ee91d7ccac548fe5bf66bc92acc2c77ea
Than0s <himanshupatil45h@gmail.com>
2024-11-28 18:47:31 +0530

    fixup! pinch zoom added in async image

--- app/src/main/java/dev/than0s/aluminium/core/composable/AluminiumAsyncImage.kt
+++ app/src/main/java/dev/than0s/aluminium/core/composable/AluminiumAsyncImage.kt
@@ -10,13 +10,23 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -27,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
@@ -35,9 +46,11 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import coil.compose.AsyncImage
 import dev.than0s.aluminium.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AluminiumAsyncImage(
     model: Any?,
@@ -53,10 +66,40 @@ fun AluminiumAsyncImage(
                 fullScreenState = false
             },
             content = {
-                PinchZoomImage(
-                    model = model
+                Surface(
+                    color = Color.Black,
+                    modifier = Modifier.fillMaxSize(),
+                    content = {
+                        Scaffold(
+                            topBar = {
+                                TopAppBar(
+                                    colors = TopAppBarDefaults.topAppBarColors(
+                                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                        titleContentColor = MaterialTheme.colorScheme.primary,
+                                    ),
+                                    title = {},
+                                    navigationIcon = {
+                                        IconButton(onClick = {
+                                            fullScreenState = false
+                                        }) {
+                                            Icon(
+                                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                                contentDescription = "Localized description"
+                                            )
+                                        }
+                                    }
+                                )
+                            }
+                        ) { contentPadding ->
+                            PinchZoomImage(
+                                model = model,
+                                modifier = Modifier.padding(contentPadding)
+                            )
+                        }
+                    }
                 )
             },
+            properties = DialogProperties(usePlatformDefaultWidth = false)
         )
     }
 
@@ -74,7 +117,8 @@ fun AluminiumAsyncImage(
 
 @Composable
 private fun PinchZoomImage(
-    model: Any?
+    model: Any?,
+    modifier: Modifier
 ) {
     var scale by remember {
         mutableFloatStateOf(1f)
@@ -84,12 +128,12 @@ private fun PinchZoomImage(
     }
 
     BoxWithConstraints(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
-            .aspectRatio(1280f / 959f)
+
     ) {
         val state = rememberTransformableState { zoomChange, panChange, rotationChange ->
-            scale = (scale * zoomChange).coerceIn(1f, 5f)
+            scale = (scale * zoomChange).coerceIn(1f, 10f)
 
             val extraWidth = (scale - 1) * constraints.maxWidth
             val extraHeight = (scale - 1) * constraints.maxHeight
