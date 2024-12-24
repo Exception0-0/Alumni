fe0a6b4bc4e8eebe3fa4fa4f5db74f1bcb808968
Than0s <himanshupatil45h@gmail.com>
2024-12-24 13:10:16 +0530

    fixup! fixup! custome app bar integrated

--- app/src/main/java/dev/than0s/aluminium/core/presentation/utils/AppBar.kt
+++ app/src/main/java/dev/than0s/aluminium/core/presentation/utils/AppBar.kt
@@ -6,8 +6,10 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -36,15 +38,21 @@ fun AluminiumTopAppBar(
                 )
             }
         },
-        actions = appBar.actions
+        actions = appBar.actions,
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        )
     )
 }
 
 private fun removeCustomAppBar() {
-    customAppBar = null
+    if (customAppBar != null) {
+        customAppBar = null
+    }
 }
 
-fun setCustomAppBar(appBar: TopAppBarItem) {
+fun addCustomAppBar(appBar: TopAppBarItem) {
     customAppBar = appBar
 }
 
