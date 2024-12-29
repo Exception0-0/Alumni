c371475937ee1441fd8e42bd8bd09714abaf9f8b
Than0s <himanshupatil45h@gmail.com>
2024-12-29 13:11:33 +0530

    post data_class fixed

--- app/src/main/java/dev/than0s/aluminium/core/domain/data_class/Post.kt
+++ app/src/main/java/dev/than0s/aluminium/core/domain/data_class/Post.kt
@@ -5,7 +5,7 @@ import android.net.Uri
 data class Post(
     val id: String = "",
     val userId: String = "",
-    val file: Uri? = Uri.EMPTY,
+    val file: Uri = Uri.EMPTY,
     val title: String = "",
     val description: String = "",
     val timestamp: Long = 0,
