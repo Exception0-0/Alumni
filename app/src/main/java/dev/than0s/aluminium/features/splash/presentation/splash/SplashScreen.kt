87ff471119e42d216eeb487358859c925828e6fc
Than0s <himanshupatil45h@gmail.com>
2024-12-31 18:09:32 +0530

    waiting animation added in splash_screen

--- app/src/main/java/dev/than0s/aluminium/features/splash/presentation/splash/SplashScreen.kt
+++ app/src/main/java/dev/than0s/aluminium/features/splash/presentation/splash/SplashScreen.kt
@@ -1,10 +1,18 @@
 package dev.than0s.aluminium.features.splash.presentation.splash
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import dev.than0s.aluminium.R
+import dev.than0s.aluminium.core.presentation.composable.AluminiumLottieAnimation
 import dev.than0s.aluminium.core.presentation.utils.Screen
-import dev.than0s.aluminium.core.presentation.composable.AluminiumTitleText
 
 @Composable
 fun SplashScreen(
@@ -16,13 +24,21 @@ fun SplashScreen(
             popAndOpen = popAndOpen
         )
     )
+    SplashScreenContent()
 }
 
 @Composable
 private fun SplashScreenContent() {
-    AluminiumTitleText(
-        title = "Loading...",
-    )
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        AluminiumLottieAnimation(
+            lottieAnimation = R.raw.waiting_animation,
+            modifier = Modifier
+                .size(180.dp)
+                .align(Alignment.Center)
+        )
+    }
 }
 
 @Preview(showSystemUi = true)
