5aa8fd98d37bc2850ff8316e48bfd4094fbbd799
Than0s <himanshupatil45h@gmail.com>
2025-01-01 14:43:02 +0530

    shimmer effect added in profile_about tab

--- app/src/main/java/dev/than0s/aluminium/core/presentation/composable/InfoFormat.kt
+++ app/src/main/java/dev/than0s/aluminium/core/presentation/composable/InfoFormat.kt
@@ -4,12 +4,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.valentinilk.shimmer.shimmer
+import dev.than0s.aluminium.ui.Size
 import dev.than0s.aluminium.ui.spacing
 import dev.than0s.aluminium.ui.textSize
 
@@ -45,4 +54,46 @@ fun CardInfoFormat(
             }
         }
     }
+}
+
+@Composable
+fun CardInfoFormatShimmer() {
+    AluminiumElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .shimmer()
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium),
+            modifier = Modifier.padding(MaterialTheme.spacing.medium)
+        ) {
+            ShimmerBackground(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(16.dp)
+            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.small),
+            ) {
+                ShimmerBackground(
+                    modifier = Modifier
+                        .height(16.dp)
+                        .width(MaterialTheme.Size.small)
+                )
+
+                ShimmerBackground(
+                    modifier = Modifier
+                        .height(16.dp)
+                        .width(MaterialTheme.Size.large)
+                )
+            }
+
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun Preview() {
+    CardInfoFormatShimmer()
 }
--- app/src/main/java/dev/than0s/aluminium/features/profile/presentation/screens/about/AboutScreen.kt
+++ app/src/main/java/dev/than0s/aluminium/features/profile/presentation/screens/about/AboutScreen.kt
@@ -2,19 +2,15 @@ package dev.than0s.aluminium.features.profile.presentation.screens.about
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import dev.than0s.aluminium.core.presentation.composable.AluminiumLinearLoading
-import dev.than0s.aluminium.core.presentation.composable.AluminumCircularLoading
 import dev.than0s.aluminium.core.presentation.composable.CardInfoFormat
+import dev.than0s.aluminium.core.presentation.composable.CardInfoFormatShimmer
 import dev.than0s.aluminium.ui.spacing
 
 @Composable
@@ -33,7 +29,7 @@ fun AboutContent(
     onEvents: (AboutEvents) -> Unit,
 ) {
     if (screenState.isLoading) {
-        AluminiumLinearLoading()
+        LoadingShimmerEffect()
     } else {
         Column(
             verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.large),
@@ -58,5 +54,16 @@ fun AboutContent(
                 )
             }
         }
+    }
+}
+
+@Composable
+private fun LoadingShimmerEffect() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.large)
+    ) {
+        CardInfoFormatShimmer()
+        CardInfoFormatShimmer()
+        CardInfoFormatShimmer()
     }
 }
