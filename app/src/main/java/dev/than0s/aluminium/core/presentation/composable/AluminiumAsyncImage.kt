7e051d6a2a75540494d9b4fe1427fe7d7fa940a4
Than0s <himanshupatil45h@gmail.com>
2025-01-02 11:07:43 +0530

    fixup! fixup! fixup! fixup! pretty_time added in post_detail

--- app/src/main/java/dev/than0s/aluminium/core/presentation/composable/AluminiumAsyncImage.kt
+++ app/src/main/java/dev/than0s/aluminium/core/presentation/composable/AluminiumAsyncImage.kt
@@ -1,19 +1,16 @@
 package dev.than0s.aluminium.core.presentation.composable
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -28,13 +25,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import coil.compose.SubcomposeAsyncImage
 import com.valentinilk.shimmer.shimmer
-import dev.than0s.aluminium.ui.roundCorners
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -156,6 +151,8 @@ private fun PinchZoomImage(
 @Composable
 private fun AsyncImageShimmerEffect() {
     ShimmerBackground(
-        modifier = Modifier.shimmer()
+        modifier = Modifier
+            .fillMaxSize()
+            .shimmer()
     )
 }
