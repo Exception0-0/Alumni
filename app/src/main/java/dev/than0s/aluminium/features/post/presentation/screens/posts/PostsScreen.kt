38c1ec437038d75f847c85940d9e790b3df52aeb
Than0s <himanshupatil45h@gmail.com>
2025-01-04 19:15:06 +0530

    fixup! pinch to zomm added in post_screen

--- app/src/main/java/dev/than0s/aluminium/features/post/presentation/screens/posts/PostsScreen.kt
+++ app/src/main/java/dev/than0s/aluminium/features/post/presentation/screens/posts/PostsScreen.kt
@@ -43,6 +43,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -123,6 +124,7 @@ private fun PostsScreenContent(
                     ) {
                         PreferredAsyncImage(
                             model = screenState.fullScreenImage,
+                            contentScale = ContentScale.Fit,
                             contentDescription = "Post Image",
                         )
                     }
