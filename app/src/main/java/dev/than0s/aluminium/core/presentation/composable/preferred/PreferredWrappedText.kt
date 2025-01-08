c95c2bea05419b0d0e1b6096c09361c8b7d5d2c5
Than0s <himanshupatil45h@gmail.com>
2025-01-08 19:50:49 +0530

    fixup! preferred_warapped_text added

--- app/src/main/java/dev/than0s/aluminium/core/presentation/composable/preferred/PreferredWrappedText.kt
+++ app/src/main/java/dev/than0s/aluminium/core/presentation/composable/preferred/PreferredWrappedText.kt
@@ -1,6 +1,7 @@
 package dev.than0s.aluminium.core.presentation.composable.preferred
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,11 +9,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import dev.than0s.aluminium.ui.textSize
-
 
 private const val maxTextLength = 100
 
@@ -25,14 +26,25 @@ fun PreferredWrappedText(
 ) {
     var isWrapped by remember { mutableStateOf(text.length > maxTextLength) }
     if (isWrapped) {
-        Text(
-            text = text.substring(0, maxTextLength) + " ...",
-            fontSize = fontSize,
-            fontWeight = fontWeight,
+        PreferredColumn(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top,
             modifier = modifier.clickable {
                 isWrapped = false
             }
-        )
+        ) {
+            Text(
+                text = text.substring(0, maxTextLength) + " ...",
+                fontSize = fontSize,
+                fontWeight = fontWeight,
+            )
+            Text(
+                text = "tap to read more".uppercase(),
+                fontSize = MaterialTheme.textSize.medium,
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Bold,
+            )
+        }
     } else {
         Text(
             text = text,
