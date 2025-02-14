9a6a66b2b86ba2a0ddfc667da74ef3e40f52c977
Than0s <himanshupatil45h@gmail.com>
2025-02-14 15:02:13 +0530

    did some_changes in appearance_screen

--- app/src/main/java/dev/than0s/aluminium/features/settings/screens/appearance/AppearanceScreen.kt
+++ app/src/main/java/dev/than0s/aluminium/features/settings/screens/appearance/AppearanceScreen.kt
@@ -1,8 +1,10 @@
 package dev.than0s.aluminium.features.settings.screens.appearance
 
+import android.os.Build
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,6 +24,7 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -129,6 +132,8 @@ private fun ThemeColumn(
     onRoundedCornersChanged: (Float) -> Unit,
     onEvent: (AppearanceScreenEvents) -> Unit,
 ) {
+    val isAndroid12Plus = remember { Build.VERSION.SDK_INT >= Build.VERSION_CODES.S }
+
     PreferredColumn(
         verticalArrangement = Arrangement.Top,
         modifier = Modifier
@@ -157,6 +162,7 @@ private fun ThemeColumn(
             trailingContent = {
                 Switch(
                     checked = isDynamicTheme,
+                    enabled = isAndroid12Plus,
                     onCheckedChange = {
                         onDynamicThemeChange(it)
                     },
@@ -193,6 +199,9 @@ private fun ThemeColumn(
             trailingContent = {
                 Switch(
                     checked = isPureBlack,
+                    enabled = ColorTheme.valueOf(currentColorTheme).let {
+                        it == ColorTheme.Dark || (it == ColorTheme.System && isSystemInDarkTheme())
+                    },
                     onCheckedChange = {
                         onPureBlackChange(it)
                     },
